716d507eb74b50285d2df9424269d9ec9b9b4186
2019166209 <ghdwns5109@naver.com>
2019-05-13 21:58:01 +0900

    dma code

--- test01.c
+++ test01.c
@@ -1,11 +1,8 @@
 #include <stdio.h>
-#include <stdint.h>
-#include <stdlib.h>
-#include <fcntl.h>
-#include <sys/mman.h>
 #include <unistd.h>
-#include <time.h>
-#include <sys/time.h>
+#include <fcntl.h>
+#include <termios.h>
+#include <sys/mman.h>
 
 //#define M_AXI_GP0_0_BASE 0x43C00000
 //#define M_AXI_GP0_0_HIGH 0x43C0FFFF
@@ -21,20 +18,18 @@
 
 #define PROT (PROT_READ|PROT_WRITE)
 #define FLAGS (MAP_SHARED)
-#define BUFF_SIZE 0xa
+#define BUFF_SIZE 0xF
 
 // MM2S CONTROL
 #define MM2S_CONTROL_REGISTER        0x00    // MM2S_DMACR
 #define MM2S_STATUS_REGISTER         0x04    // MM2S_DMASR
 #define MM2S_SOURCE_ADDRESS          0x18    // MM2S_SA
-#define MM2S_SOURCE_ADDRESS_MSB      0x1C    // MM2S_SA_MSB
 #define MM2S_LENGTH                  0x28    // MM2S_LENGTH
 
 // S2MM CONTROL
 #define S2MM_CONTROL_REGISTER        0x30    // S2MM_DMACR
 #define S2MM_STATUS_REGISTER         0x34    // S2MM_DMASR
 #define S2MM_DESTINATION_ADDRESS     0x48    // S2MM_DA
-#define S2MM_DESTINATION_ADDRESS_MSB 0x4C    // S2MM_DA_MSB
 #define S2MM_LENGTH                  0x58    // S2MM_LENGTH
 
 //static volatile unsigned int *dram_source = NULL;
@@ -42,12 +37,50 @@
 //static volatile unsigned int *m_axi_base = NULL;
 //static volatile unsigned int *dma_base = NULL;
 
-static volatile unsigned int* dram_source = NULL;
-static volatile unsigned int* dram_target = NULL;
-static volatile unsigned int* dma_base = NULL;
+
+void dma_s2mm_status(unsigned int* dma_virtual_address) {
+    unsigned int status = dma_virtual_address[S2MM_STATUS_REGISTER>>2];
+    printf("Stream to memory-mapped status (0x%08x@0x%02x):", status, S2MM_STATUS_REGISTER);
+    if (status & 0x00000001) printf(" halted"); else printf(" running");
+    if (status & 0x00000002) printf(" idle");
+    if (status & 0x00000008) printf(" SGIncld");
+    if (status & 0x00000010) printf(" DMAIntErr");
+    if (status & 0x00000020) printf(" DMASlvErr");
+    if (status & 0x00000040) printf(" DMADecErr");
+    if (status & 0x00000100) printf(" SGIntErr");
+    if (status & 0x00000200) printf(" SGSlvErr");
+    if (status & 0x00000400) printf(" SGDecErr");
+    if (status & 0x00001000) printf(" IOC_Irq");
+    if (status & 0x00002000) printf(" Dly_Irq");
+    if (status & 0x00004000) printf(" Err_Irq");
+    printf("\n");
+}
+
+void dma_mm2s_status(unsigned int* dma_virtual_address) {
+    unsigned int status = dma_virtual_address[MM2S_STATUS_REGISTER>>2];
+    printf("Memory-mapped to stream status (0x%08x@0x%02x):", status, MM2S_STATUS_REGISTER);
+    if (status & 0x00000001) printf(" halted"); else printf(" running");
+    if (status & 0x00000002) printf(" idle");
+    if (status & 0x00000008) printf(" SGIncld");
+    if (status & 0x00000010) printf(" DMAIntErr");
+    if (status & 0x00000020) printf(" DMASlvErr");
+    if (status & 0x00000040) printf(" DMADecErr");
+    if (status & 0x00000100) printf(" SGIntErr");
+    if (status & 0x00000200) printf(" SGSlvErr");
+    if (status & 0x00000400) printf(" SGDecErr");
+    if (status & 0x00001000) printf(" IOC_Irq");
+    if (status & 0x00002000) printf(" Dly_Irq");
+    if (status & 0x00004000) printf(" Err_Irq");
+    printf("\n");
+}
 
 int main(){
 	
+//	unsigned int* phy_dram_source = NULL;
+//	unsigned int* phy_dram_target = NULL;
+	unsigned int* phy_dram_base = NULL;
+	unsigned int* phy_dram_base_t = NULL;
+	unsigned int* phy_dma_base = NULL;
 
 	int fd = open("/dev/mem",(O_RDWR|O_SYNC));
 	if(fd == -1){
@@ -61,11 +94,116 @@ int main(){
 //		printf("ERROR: m_axi_gp mmap() failed..." );
 //		return 1;
 //	}
-	
-	unsigned int *virtual_dram_base;
-	virtual_dram_base = mmap(NULL, DRAM_MASK, PROT, FLAGS, fd, DRAM_BASE);
-	if(virtual_dram_base == MAP_FAILED){
+
+	phy_dram_base = mmap(NULL, 0xffff, PROT, FLAGS, fd, 0x1e000000);
+	if(phy_dram_base == MAP_FAILED){
 		printf("ERROR: dram mmap() failed...\n" );
+		return 1;
+	}	
+	
+	phy_dram_base_t = mmap(NULL, 0xffff, PROT, FLAGS, fd, 0x0f000000);
+	if(phy_dram_base_t == MAP_FAILED){
+		printf("ERROR: dram_t mmap() failed...\n" );
+		return 1;
+	}
+	
+	phy_dma_base = mmap(NULL, DMA_MASK, PROT, FLAGS, fd, DMA_BASE);
+	if(phy_dma_base == MAP_FAILED){
+		printf("ERROR: dram mmap() failed...\n" );
+		return 1;
+	}
+
+	unsigned int i;
+	printf("*************** before init, read dram source **************\n");
+	for ( i = 0; i < (BUFF_SIZE/4)+4; i++) printf("data[%08d] : %08x\n", i, phy_dram_base[i<<2]);
+
+	printf("initializing dram source ...\n");
+	for ( i = 0; i < (BUFF_SIZE/4); i++) phy_dram_base[i<<2] = 0x22222222;
+
+	printf("*************** after init, read dram source ***************\n");
+	for ( i = 0; i < (BUFF_SIZE/4)+4; i++) printf("data[%08d] : %08x\n", i, phy_dram_base[i<<2]);
+
+	printf("*************** before init, read dram target ***************\n");
+	for ( i = 0; i < (BUFF_SIZE/4)+4; i++) printf("data[%08d] : %08x\n", i, phy_dram_base_t[i<<2]);
+
+	printf("initializing dram target ...\n");
+	for ( i = 0; i < (BUFF_SIZE/4); i++) phy_dram_base_t[i<<2] = 0xDEADBEEF;
+
+	printf("*************** after init, read dram target ***************\n");
+	for ( i = 0; i < (BUFF_SIZE/4)+4; i++) printf("data[%08d] : %08x\n", i, phy_dram_base_t[i<<2]);
+	
+	printf("---------------- using dma copy the data of dram source addr to dram target addr --------------------\n");
+
+	printf("Resetting DMA\n");
+	phy_dma_base[MM2S_CONTROL_REGISTER >> 2] = 4;
+	phy_dma_base[S2MM_CONTROL_REGISTER >> 2] = 4;
+	dma_s2mm_status(phy_dma_base);
+	dma_mm2s_status(phy_dma_base);
+
+	printf("Halting DMA\n");
+	phy_dma_base[MM2S_CONTROL_REGISTER >> 2] = 0;
+	phy_dma_base[S2MM_CONTROL_REGISTER >> 2] = 0;
+	dma_s2mm_status(phy_dma_base);
+	dma_mm2s_status(phy_dma_base);
+
+	printf("writing destination address...\n");
+	phy_dma_base[S2MM_DESTINATION_ADDRESS >> 2] = 0x0f000000;
+	dma_s2mm_status(phy_dma_base);
+
+	printf("writing source address...\n");
+	phy_dma_base[MM2S_SOURCE_ADDRESS >> 2] = 0x1e000000;
+	dma_mm2s_status(phy_dma_base);
+
+	printf("Starting S2MM channel with all interrupts masked...\n");
+	phy_dma_base[S2MM_CONTROL_REGISTER >> 2] = 0xF001;
+	dma_s2mm_status(phy_dma_base);
+
+	printf("Starting MM2S channel with all interrupts masked...\n");
+	phy_dma_base[MM2S_CONTROL_REGISTER >> 2] = 0xF001;
+	dma_mm2s_status(phy_dma_base);
+
+	printf("writing s2mm length...\n");
+	phy_dma_base[S2MM_LENGTH >> 2] = 32;
+	dma_s2mm_status(phy_dma_base);
+
+	printf("writing mm2s length...\n");
+	phy_dma_base[MM2S_LENGTH >> 2] = 32;
+	dma_mm2s_status(phy_dma_base);
+
+	unsigned int mm2s_status = phy_dma_base[MM2S_STATUS_REGISTER >> 2];
+	unsigned int s2mm_status = phy_dma_base[S2MM_STATUS_REGISTER >> 2];
+
+	printf("mm2s sync...\n");
+	while (!(mm2s_status & 1<<12) || !(mm2s_status & 1<<1))
+	{
+		//dma_mm2s_status(phy_dma_base);
+		mm2s_status = phy_dma_base[MM2S_STATUS_REGISTER >> 2];
+	}
+	printf("s2mm sync...\n");
+	while (!(s2mm_status & 0x1000) || !(s2mm_status & 0x2))
+	{
+		//dma_s2mm_status(phy_dma_base);
+		s2mm_status = phy_dma_base[S2MM_STATUS_REGISTER >> 2];
+	}
+
+	printf("mm2s_status : %08x\n", mm2s_status);
+	printf("s2mm_status : %08x\n", s2mm_status);
+	printf("re-read dram from target base to target base + BUFF_SIZE ***************\n");
+	for ( i = 0; i < (BUFF_SIZE/4)+4; i++) printf("data[%08d] : %08x\n", i, phy_dram_base_t[i<<2]);
+
+	
+/*
+	unsigned int *virtual_dram_base_s;
+	virtual_dram_base_s = mmap(NULL, DRAM_MASK, PROT, FLAGS, fd, DRAM_BASE);
+	if(virtual_dram_base_s == MAP_FAILED){
+		printf("ERROR: dram_s mmap() failed...\n" );
+		return 1;
+	}
+	
+	unsigned int *virtual_dram_base_t;
+	virtual_dram_base_t = mmap(NULL, DRAM_MASK, PROT, FLAGS, fd, DRAM_BASE+0x10000);
+	if(virtual_dram_base_t == MAP_FAILED){
+		printf("ERROR: dram_t mmap() failed...\n" );
 		return 1;
 	}
 	
@@ -75,131 +213,95 @@ int main(){
 		printf("ERROR: dma mmap() failed...\n" );
 		return 1;
 	}
-
+*/
 //	m_axi_base = (volatile unsigned int *)virtual_m_axi_gp0_0_base + (unsigned long) 0x0;
-	dram_source = (volatile unsigned int *) virtual_dram_base + (unsigned int) 0x0;
-	dram_target = (volatile unsigned int *)virtual_dram_base + (unsigned int) 0x10000;
-	dma_base = (volatile unsigned int *)virtual_dma_base + (unsigned int) 0x0;
+//	phy_dram_source = (volatile unsigned int *) virtual_dram_base + (unsigned int) 0x0;
+//	phy_dram_target = (volatile unsigned int *)virtual_dram_base + (unsigned int) 0x10000;
+//	phy_dma_base = (volatile unsigned int *)virtual_dma_base;
 
-	printf(" reset dma ... \n");
-	dma_base[MM2S_CONTROL_REGISTER >> 2] = 0x4;
-	dma_base[S2MM_CONTROL_REGISTER >> 2] = 0x4;
-	dma_base[MM2S_CONTROL_REGISTER >> 2] = 0x0;
-	dma_base[S2MM_CONTROL_REGISTER >> 2] = 0x0;
-	printf(" done ... \n");
+//	phy_dram_source = (unsigned int *) virtual_dram_base_s;
+//	phy_dram_target = (unsigned int *)virtual_dram_base_t;
+//	phy_dma_base = (unsigned int *)virtual_dma_base;
+/*
+	unsigned int i;
+	printf("*************** before init, read dram source **************\n");
+	for ( i = 0; i < (BUFF_SIZE/4)+4; i++) printf("data[%08d] : %08x\n", i, phy_dram_source[i<<2]);
 
-	int i;
-	printf("*************** before init, read dram from source base to source base + BUFF_SIZE ***************\n");
-	for ( i = 0; i < (BUFF_SIZE/4)+4; i++) printf("data[%08d] : %08x\n", i, dram_source[i<<2]);
+	printf("initializing dram source ...\n");
+	for ( i = 0; i < (BUFF_SIZE/4); i++) phy_dram_source[i<<2] = 0x22222222;
 
-	printf("initializing dram from source base to source base + BUFF_SIZE ...\n");
-	for ( i = 0; i < (BUFF_SIZE/4); i++) dram_source[i<<2] = 0x22222222;
-
-	printf("*************** after init, read dram from source base to source base + BUFF_SIZE ***************\n");
-	for ( i = 0; i < (BUFF_SIZE/4)+4; i++) printf("data[%08d] : %08x\n", i, dram_source[i<<2]);
+	printf("*************** after init, read dram source ***************\n");
+	for ( i = 0; i < (BUFF_SIZE/4)+4; i++) printf("data[%08d] : %08x\n", i, phy_dram_source[i<<2]);
 
 	printf("\n");
 
-	printf("*************** before init, read dram from target base to target base + BUFF_SIZE ***************\n");
-	for ( i = 0; i < (BUFF_SIZE/4)+4; i++) printf("data[%08d] : %08x\n", i, dram_target[i<<2]);
+	printf("*************** before init, read dram target ***************\n");
+	for ( i = 0; i < (BUFF_SIZE/4)+4; i++) printf("data[%08d] : %08x\n", i, phy_dram_target[i<<2]);
 
-	printf("initializing dram from target base to target base + BUFF_SIZE ...\n");
-	for ( i = 0; i < (BUFF_SIZE/4); i++) dram_target[i<<2] = 0xDEADBEEF;
+	printf("initializing dram target ...\n");
+	for ( i = 0; i < (BUFF_SIZE/4); i++) phy_dram_target[i<<2] = 0xDEADBEEF;
 
-	printf("*************** after init, read dram from target base to target base + BUFF_SIZE ***************\n");
-	for ( i = 0; i < (BUFF_SIZE/4)+4; i++) printf("data[%08d] : %08x\n", i, dram_target[i<<2]);
-
+	printf("*************** after init, read dram target ***************\n");
+	for ( i = 0; i < (BUFF_SIZE/4)+4; i++) printf("data[%08d] : %08x\n", i, phy_dram_target[i<<2]);
+	
 	printf("---------------- using dma copy the data of dram source addr to dram target addr --------------------\n");
-	
-	int mm2s_status, s2mm_status;
 
-	dma_base[MM2S_SOURCE_ADDRESS >> 2] = DRAM_BASE;
-	dma_base[MM2S_LENGTH >> 2] = BUFF_SIZE;
-	dma_base[S2MM_DESTINATION_ADDRESS >> 2] = DRAM_BASE + 0x10000;
-	dma_base[S2MM_LENGTH >> 2] = BUFF_SIZE;
-	
-	mm2s_status = dma_base[MM2S_STATUS_REGISTER >> 2];
-	s2mm_status = dma_base[S2MM_STATUS_REGISTER >> 2];
-	printf("mm2s_status : %08x\n", mm2s_status);
-	printf("s2mm_status : %08x\n", s2mm_status);
+	printf("Resetting DMA\n");
+	phy_dma_base[MM2S_CONTROL_REGISTER >> 2] = 0x4;
+	phy_dma_base[S2MM_CONTROL_REGISTER >> 2] = 0x4;
+	dma_s2mm_status(phy_dma_base);
+	dma_mm2s_status(phy_dma_base);
 
-	printf("dma sets to running...\n");
-	dma_base[MM2S_CONTROL_REGISTER ] = 0x1;
-	dma_base[S2MM_CONTROL_REGISTER ] = 0x1;
+	printf("Halting DMA\n");
+	phy_dma_base[MM2S_CONTROL_REGISTER >> 2] = 0x0;
+	phy_dma_base[S2MM_CONTROL_REGISTER >> 2] = 0x0;
+	dma_s2mm_status(phy_dma_base);
+	dma_mm2s_status(phy_dma_base);
 
-	int controlregister_ok =0;
-/*
-	while (!controlregister_ok)
-    {
-	mm2s_status = dma_base[MM2S_STATUS_REGISTER >> 2];
-	s2mm_status = dma_base[S2MM_STATUS_REGISTER >> 2];
-		controlregister_ok = ((mm2s_status & 0x00001000) && (s2mm_status & 0x00001000));
-		printf("Memory-mapped to stream status (0x%08x@0x%02x):\n", mm2s_status, MM2S_STATUS_REGISTER);
-		printf("MM2S_STATUS_REGISTER status register values:\n");
-		if (mm2s_status & 0x00000001) printf(" halted"); else printf(" running");
-		if (mm2s_status & 0x00000002) printf(" idle");
-		if (mm2s_status & 0x00000008) printf(" SGIncld");
-		if (mm2s_status & 0x00000010) printf(" DMAIntErr");
-		if (mm2s_status & 0x00000020) printf(" DMASlvErr");
-		if (mm2s_status & 0x00000040) printf(" DMADecErr");
-		if (mm2s_status & 0x00000100) printf(" SGIntErr");
-		if (mm2s_status & 0x00000200) printf(" SGSlvErr");
-		if (mm2s_status & 0x00000400) printf(" SGDecErr");
-		if (mm2s_status & 0x00001000) printf(" IOC_Irq");
-		if (mm2s_status & 0x00002000) printf(" Dly_Irq");
-		if (mm2s_status & 0x00004000) printf(" Err_Irq");
-		printf("\n");
-		printf("Stream to memory-mapped status (0x%08x@0x%02x):\n", s2mm_status, S2MM_STATUS_REGISTER);
-		printf("S2MM_STATUS_REGISTER status register values:\n");
-		if (s2mm_status & 0x00000001) printf(" halted"); else printf(" running");
-		if (s2mm_status & 0x00000002) printf(" idle");
-		if (s2mm_status & 0x00000008) printf(" SGIncld");
-		if (s2mm_status & 0x00000010) printf(" DMAIntErr");
-		if (s2mm_status & 0x00000020) printf(" DMASlvErr");
-		if (s2mm_status & 0x00000040) printf(" DMADecErr");
-		if (s2mm_status & 0x00000100) printf(" SGIntErr");
-		if (s2mm_status & 0x00000200) printf(" SGSlvErr");
-		if (s2mm_status & 0x00000400) printf(" SGDecErr");
-		if (s2mm_status & 0x00001000) printf(" IOC_Irq");
-		if (s2mm_status & 0x00002000) printf(" Dly_Irq");
-		if (s2mm_status & 0x00004000) printf(" Err_Irq");
-		printf("\n");
-    }
-*/
+	printf("writing destination address...\n");
+	phy_dma_base[S2MM_DESTINATION_ADDRESS >> 2] = 0x10000;
+	dma_s2mm_status(phy_dma_base);
+
+	printf("writing source address...\n");
+	phy_dma_base[MM2S_SOURCE_ADDRESS >> 2] = 0x00;
+	dma_mm2s_status(phy_dma_base);
+
+	printf("Starting S2MM channel with all interrupts masked...\n");
+	phy_dma_base[S2MM_CONTROL_REGISTER >> 2] = 0xF001;
+	dma_s2mm_status(phy_dma_base);
+
+	printf("Starting MM2S channel with all interrupts masked...\n");
+	phy_dma_base[MM2S_CONTROL_REGISTER >> 2] = 0xF001;
+	dma_mm2s_status(phy_dma_base);
+
+	printf("writing s2mm length...\n");
+	phy_dma_base[S2MM_LENGTH >> 2] = 32;
+	dma_s2mm_status(phy_dma_base);
+
+	printf("writing mm2s length...\n");
+	phy_dma_base[MM2S_LENGTH >> 2] = 32;
+	dma_mm2s_status(phy_dma_base);
+
+	unsigned int mm2s_status = phy_dma_base[MM2S_STATUS_REGISTER >> 2];
+	unsigned int s2mm_status = phy_dma_base[S2MM_STATUS_REGISTER >> 2];
+
+	printf("mm2s sync...\n");
+	while (!(mm2s_status & 1<<12) || !(mm2s_status & 1<<1))
+	{
+		//dma_mm2s_status(phy_dma_base);
+		mm2s_status = phy_dma_base[MM2S_STATUS_REGISTER >> 2];
+	}
+	printf("s2mm sync...\n");
+	while (!(s2mm_status & 0x1000) || !(s2mm_status & 0x2))
+	{
+		//dma_s2mm_status(phy_dma_base);
+		s2mm_status = phy_dma_base[S2MM_STATUS_REGISTER >> 2];
+	}
+
 	printf("mm2s_status : %08x\n", mm2s_status);
 	printf("s2mm_status : %08x\n", s2mm_status);
 	printf("re-read dram from target base to target base + BUFF_SIZE ***************\n");
-	for ( i = 0; i < (BUFF_SIZE/4)+4; i++) printf("data[%08d] : %08x\n", i, dram_target[i<<2]);
-
-
-
-/*
-	printf("*************" << "read ddr..." << "*************"<<endl;
-	for(int i = 0; i < BUFF_SIZE; i++)
-		printf("\t*ddr_base : " << std::hex << *(ddr_base+i) << std::dec );
-	printf(endl;
-	
-	printf("*************" << "read dma..." << "*************"<<endl;
-	for(int i = 0; i < BUFF_SIZE; i++)
-		printf("\t*dma_base : " << std::hex << *(dma_base+i) << std::dec );
-	printf(endl;
-
-	printf("*****************" << "writing to m_axi_wdata..." << "******************"<<endl;
-	volatile unsigned int *m_axi_wdata = NULL;
-	m_axi_wdata = m_axi_base;
-//	for(unsigned int i = 0; i<BUFF_SIZE; i++){
-	*m_axi_wdata = 0x7;
-//	}
-
-	printf("************************************" << "re-read ddr..." << "************************************"<<endl;
-	for(int i = 0; i < BUFF_SIZE; i++)
-		printf("\t*ddr_base : " << std::hex << *(ddr_base+i) << std::dec );
-	printf(endl;
-
-	printf("************************************" << "re-read dma..." << "************************************"<<endl;
-	for(int i = 0; i < BUFF_SIZE; i++)
-		printf("\t*dma_base : " << std::hex << *(dma_base+i) << std::dec );
-	printf(endl;
-*/
+	for ( i = 0; i < (BUFF_SIZE/4)+4; i++) printf("data[%08d] : %08x\n", i, phy_dram_target[i<<2]);
+*/	
 	return 0;
 }
